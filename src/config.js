07173d4bfeedd2aa300c9c49b98880cff64d23c4
Thinute <tongthanhthao2009@gmail.com>
2024-11-15 10:05:03 +0700

    assign

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    databaseURL: 'http://localhost:27017', // URL
-    của cơ sở dữ liệu
-    itemsPerPage: 5
+    allowAssignment: true, // Bật tính năng gán công việc
+    itemsPerPage: 15
     };
